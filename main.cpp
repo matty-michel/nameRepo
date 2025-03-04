eeee48a3fc55a02deef8e001a1e12edc8b4c5631
matty-michel <mmichel@carthage.edu>
2025-03-04 14:50:30 -0600

    added calculate_age function

--- main.cpp
+++ main.cpp
@@ -5,10 +5,16 @@ int main()
 {
     //cout << "GitHub Demo" << endl;
     void print_name();
+    void calculate_age(2025);
     return 0;
 }
 
 //--
 void print_name() {
     cout << "Matt Michel" << endl;
+}
+//--
+void calculate_age(int year) {
+    int age = (year - 2003);
+    cout << "(" << age << ")" << endl
 }
